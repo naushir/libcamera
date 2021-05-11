naushir/libcamera

commit fcfb1dc02a6baed23a85aa1186dc8aa9b2217f49

libcamera: raspberry: Report sensor timestamp
Report the sensor's timestamp in the Request metadata by using the
Unicam::Image buffer timestamp as an initial approximation.

The buffer's timestamp is recorded at DMA-transfer time, and it does not
theoretically matches the 'start of exposure' definition, but when used
to compare two consecutive frames it gives an acceptable estimation of
the sensor frame period duration.

Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Naushir Patuck <naush@raspberrypi.com>
Signed-off-by: Jacopo Mondi <jacopo@jmondi.org>

src/libcamera/pipeline/raspberrypi/raspberrypi.cpp
@@ -1410,6 +1410,18 @@ void RPiCameraData::unicamBufferDequeue(FrameBuffer *buffer)
 
 	if (stream == &unicam_[Unicam::Image]) {
 		/*
+		 * Record the sensor timestamp in the Request.
+		 *
+		 * \todo Do not assume the request in the front of the queue
+		 * is the correct one
+		 */
+		Request *request = requestQueue_.front();
+		ASSERT(request);
+
+		request->metadata().set(controls::SensorTimestamp,
+					buffer->metadata().timestamp);
+
+		/*
 		 * Lookup the sensor controls used for this frame sequence from
 		 * DelayedControl and queue them along with the frame buffer.
 		 */

commit 7139a5d68a8294915afd4ded1804db8d3b1ab578

pipeline: raspberrypi: Store timestamp in the correct Request metadata
Write the controls::SensorTimestamp value in the Request metadata when
the request is popped from the queue ready to run the pipeline. This
ensures that the timestamp is written to the correct Request item,
which may not be at the top of the queue when the Unicam buffer dequeue
occurs.

Fixes: fcfb1dc02a6b ("libcamera: raspberry: Report sensor timestamp")
Signed-off-by: Naushir Patuck <naush@raspberrypi.com>

src/libcamera/pipeline/raspberrypi/raspberrypi.cpp
@@ -221,6 +221,8 @@ public:
 
 private:
 	void checkRequestCompleted();
+	void fillRequestMetadata(const ControlList &bufferControls,
+				 Request *request);
 	void tryRunPipeline();
 	bool findMatchingBuffers(BayerFrame &bayerFrame, FrameBuffer *&embeddedBuffer);
 
@@ -1417,18 +1419,6 @@ void RPiCameraData::unicamBufferDequeue(FrameBuffer *buffer)
 
 	if (stream == &unicam_[Unicam::Image]) {
 		/*
-		 * Record the sensor timestamp in the Request.
-		 *
-		 * \todo Do not assume the request in the front of the queue
-		 * is the correct one
-		 */
-		Request *request = requestQueue_.front();
-		ASSERT(request);
-
-		request->metadata().set(controls::SensorTimestamp,
-					buffer->metadata().timestamp);
-
-		/*
 		 * Lookup the sensor controls used for this frame sequence from
 		 * DelayedControl and queue them along with the frame buffer.
 		 */
@@ -1689,6 +1679,13 @@ void RPiCameraData::applyScalerCrop(const ControlList &controls)
 	}
 }
 
+void RPiCameraData::fillRequestMetadata(const ControlList &bufferControls,
+					Request *request)
+{
+	request->metadata().set(controls::SensorTimestamp,
+				bufferControls.get(controls::SensorTimestamp));
+}
+
 void RPiCameraData::tryRunPipeline()
 {
 	FrameBuffer *embeddedBuffer;
@@ -1707,6 +1704,14 @@ void RPiCameraData::tryRunPipeline()
 
 	/* See if a new ScalerCrop value needs to be applied. */
 	applyScalerCrop(request->controls());
+
+	/*
+	 * Clear the request metadata and fill it with some initial non-IPA
+	 * related controls. We clear it first because the request metadata
+	 * may have been populated if we have dropped the previous frame.
+	 */
+	request->metadata().clear();
+	fillRequestMetadata(bayerFrame.controls, request);
 
 	/*
 	 * Process all the user controls by the IPA. Once this is complete, we