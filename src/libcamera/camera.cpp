73649f2132910042e3454e723990785e92116b79
David Plowman <david.plowman@raspberrypi.com>
2022-09-28 15:43:38 +0100

    Update Request and Camera classes to support submit and sync ids

--- src/libcamera/camera.cpp
+++ src/libcamera/camera.cpp
@@ -1151,6 +1151,12 @@ int Camera::queueRequest(Request *request)
 		}
 	}
 
+	if (!request->controls().empty())
+		requestSubmitId_++;
+	request->submitId = requestSubmitId_;
+	request->controlListId = request->submitId;
+	request->syncId = 0;
+
 	d->pipe_->invokeMethod(&PipelineHandler::queueRequest,
 			       ConnectionTypeQueued, request);
 
@@ -1186,6 +1192,8 @@ int Camera::start(const ControlList *controls)
 	LOG(Camera, Debug) << "Starting capture";
 
 	ASSERT(d->requestSequence_ == 0);
+
+	requestSubmitId_ = 0;
 
 	ret = d->pipe_->invokeMethod(&PipelineHandler::start,
 				     ConnectionTypeBlocking, this, controls);
